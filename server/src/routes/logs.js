49bb5893364c42d925a802845ae7dcabcc114f62
leon Boakye-Adjei <leon_adjei@hotmail.com>
2021-03-13 10:43:53 +0000

    added gitignore

--- server/src/routes/logs.js
+++ server/src/routes/logs.js
@@ -3,10 +3,9 @@ const router = Router();
 
 const LogEntry = require('../models/LogEntry');
 
-router.get('/', (req, res) => {
-	res.json({
-		message: 'GET ROUTE >>>',
-	});
+router.get('/', async (req, res) => {
+	const entries = await LogEntry.find();
+	res.json(entries);
 });
 
 router.post('/', async (req, res, next) => {
