c6564764eee9ca33028eb4021195e1e3090e3d62
leon Boakye-Adjei <leon_adjei@hotmail.com>
2021-03-16 07:06:03 +0000

    added popup modal to marker

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import react from 'react';
 import { useState, useEffect } from 'react';
-import ReactMapGL, { Marker } from 'react-map-gl';
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 import { listLogEntries } from './API';
 
 function App() {
+	const [showPopup, setShowPopup] = useState({});
 	const [logEntries, setLogEntries] = useState([]);
 	const [viewport, setViewport] = useState({
 		width: '100vw',
@@ -28,31 +29,52 @@ function App() {
 			onViewportChange={(nextViewport) => setViewport(nextViewport)}
 		>
 			{logEntries.map((entry) => (
-				<Marker
-					key={entry._id}
-					latitude={entry.latitude}
-					longitude={entry.longitude}
-					offsetLeft={-20}
-					offsetTop={-10}
-				>
-					<div>
-						<svg
-							className='marker'
-							style={{
-								width: viewport.zoom,
-							}}
-							viewBox='0 0 24 24'
-							stroke-width='1.5'
-							fill='none'
-							stroke-linecap='round'
-							stroke-linejoin='round'
-							class='css-i6dzq1'
+				<>
+					<Marker
+						key={entry._id}
+						latitude={entry.latitude}
+						longitude={entry.longitude}
+						offsetLeft={-12}
+						offsetTop={-24}
+					>
+						<div
+							onClick={() =>
+								setShowPopup({
+									showPopup,
+									[entry._id]: true,
+								})
+							}
 						>
-							<path d='M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z'></path>
-							<circle cx='12' cy='10' r='3'></circle>
-						</svg>
-					</div>
-				</Marker>
+							<svg
+								className='marker'
+								style={{
+									width: '24px',
+									height: '24px',
+								}}
+								viewBox='0 0 24 24'
+								stroke-width='1.5'
+								fill='none'
+								stroke-linecap='round'
+								stroke-linejoin='round'
+							>
+								<path d='M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z'></path>
+								<circle cx='12' cy='10' r='3'></circle>
+							</svg>
+						</div>
+					</Marker>
+					{showPopup[entry._id] ? (
+						<Popup
+							latitude={entry.latitude}
+							longitude={entry.longitude}
+							closeButton={true}
+							closeOnClick={false}
+							onClose={() => this.setState({ showPopup: false })}
+							anchor='top'
+						>
+							<div>you are here</div>
+						</Popup>
+					) : null}
+				</>
 			))}
 		</ReactMapGL>
 	);
