c989b038b3832a33390e7124e06fcbebe8980cad
leon Boakye-Adjei <leon_adjei@hotmail.com>
2021-04-18 11:53:43 +0100

    adding api-key to form

--- client/src/API.js
+++ client/src/API.js
@@ -6,12 +6,22 @@ export async function listLogEntries() {
 }
 
 export async function createLogEntry(entry) {
+	const apiKey = entry.apiKey;
+	delete entry.apiKey;
 	const res = await fetch(`${API_URL}/api/logs`, {
 		method: 'POST',
 		headers: {
 			'content-type': 'application/json',
+			'X-API-KEY': apiKey,
 		},
 		body: JSON.stringify(entry),
 	});
-	return res.json();
+	const json = await res.json();
+	if (res.ok) {
+		return json;
+	}
+
+	const error = new Error(json.message);
+	error.res = json;
+	throw error;
 }
